5d3656bda3853fbd76324c36238b4365016b13f1
Martin <a@b.com>
2023-10-01 23:38:53 -0400

    Done

--- src/Point.java
+++ src/Point.java
@@ -12,7 +12,53 @@ public class Point {
     }
 
     public Point() {
-        this.x = 5; this.y = 5;
+        this.x = 0; this.y = 0;
     }
 
+    public int getX() {
+        return this.x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String coordinate() {
+        return String.format("(%d, %d)", this.x, this.y);
+    }
+
+    public String quadrant() {
+        //0,0 = origin
+        //any,0 = on axis
+        //pos,pos = I
+        //neg,pos = II
+        //neg,neg = III
+        //pos,neg = IV
+
+
+        if (this.x == 0 | this.y == 0) {
+            return "on an axis";
+        }
+        if (this.x == 0 && this.y == 0) {
+            return "I";
+        }
+        if (this.x > 0 && this.y > 0) {
+            return "I";
+        }
+        if (this.x < 0 && this.y > 0) {
+            return "II";
+        }
+        if (this.x < 0 && this.y < 0) {
+            return "III";
+        }
+        if (this.x > 0 && this.y < 0) {
+            return "IV";
+        }
+        return "error";
+    }
 }
